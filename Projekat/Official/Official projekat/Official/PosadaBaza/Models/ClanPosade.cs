4972cf79803292cdd50d2d708f898df72ca120f5
Dino Zivojevic <dzivojevic1@etf.unsa.ba>
2016-06-02 19:21:37 +0200

    dodan projekat iznova

---
+++ Projekat/Official/Official projekat/Official/PosadaBaza/Models/ClanPosade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Official.PosadaBaza.Models
+{
+    class ClanPosade
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }//primary key u bazi
+        public string fourSqaureId { get; set; }//trebati ce za sihronizaciju kasnije
+        public string Ime { get; set; } //Ime clana posade u bazu podataka
+        public string Prezime { get; set; } //Prezime clana posade u bazi podataka
+    }
+}
---
+++ Projekat/Official/Solution komponente/Help.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Official
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Help : Page
+    {
+        public Help()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Views.UnosPosade));
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Views.UnosParametara));
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Views.PrikazParametara));
+        }
+    }
+}
